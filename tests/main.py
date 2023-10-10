e6660462ce5faeeecb3b182b454b2426bb51750d
Paul Li <shi.li.314159@gmail.com>
2023-10-09 21:05:43 -0400

    Edit tests to account for package in pypi

--- tests/main.py
+++ tests/main.py
@@ -1,9 +1,4 @@
-from pathlib import Path
-import sys
-import os
-sys.path.insert(0, os.path.join(Path(os.path.dirname(os.path.abspath(__file__))).parent.resolve()))
-
-from src.spire_wrapper import api
+from spire_wrapper import api
 
 import asyncio
 
